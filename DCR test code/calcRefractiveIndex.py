1a74e7d09c4bc1b2f38c17bee09b1413b3177660
Ian Sullivan <sullivan@D-173-250-202-149.dhcp4.washington.edu>
2015-09-29 17:35:29 -0700

    DCR bug fixes: last commit had typo, unit error

--- DCR test code/calcRefractiveIndex.py
+++ DCR test code/calcRefractiveIndex.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def p_s(atmospheric_pressure, water_vapor_pressure = 0.):
+def p_s(atmospheric_pressure, water_vapor_pressure = 0.02):
 #
 # both atmospheric_pressure and water_vapor_pressure are input in atmospheres.
 # output is converted to millibars
@@ -11,7 +11,7 @@ def p_s(atmospheric_pressure, water_vapor_pressure = 0.):
 	return effective_pressure
 
 
-def p_w(water_vapor_pressure = 0.):
+def p_w(water_vapor_pressure = 0.02):
 	water_vapor_pressure_millibar = 760. * 1.333224 * water_vapor_pressure
 	return water_vapor_pressure_millibar
 
@@ -20,18 +20,21 @@ def d_s(atmospheric_pressure, water_vapor_pressure, temperature):
 	# calculate dry air pressure term to refractive index calculation
 	T_use = 273.15 + temperature
 
-	return (1. + p_s(atmospheric_pressure, water_vapor_pressure) * 
-		(57.90E-8 - (9.3250E-4 / T_use) + (0.25844 / np.power(T_use, 2.))) * 
-		p_s(atmospheric_pressure, water_vapor_pressure) / T_use)
+	eqn_1 = (1. + p_s(atmospheric_pressure, water_vapor_pressure) * 
+		(57.90E-8 - (9.3250E-4 / T_use) + (0.25844 / np.power(T_use, 2.))))
+
+	return eqn_1 * p_s(atmospheric_pressure, water_vapor_pressure) / T_use
 
 
 def d_w(water_vapor_pressure, temperature):
 	# calculate water vapor pressure term to refractive index calculation
 	T_use =	273.15 + temperature
 
-	return ((1. + p_w(water_vapor_pressure) * (1. + 3.7E-4 * p_w(water_vapor_pressure))) * 
-			(-2.37321E-3 + (2.23366 / T_use) - (710.792 / np.power(T_use, 2.)) + 
-			(7.75141E-4 / np.power(T_use, 3.))) * p_w(water_vapor_pressure) / T_use)
+	eqn_1 = (-2.37321E-3 + (2.23366 / T_use) - (710.792 / np.power(T_use, 2.)) + 
+			(7.75141E-4 / np.power(T_use, 3.)))
+	eqn_2 = 1. + p_w(water_vapor_pressure) * (1. + 3.7E-4 * p_w(water_vapor_pressure)) * eqn_1
+
+	return eqn_2 * p_w(water_vapor_pressure) / T_use
 
 
 def n_delta(wavelength, atmospheric_pressure, water_vapor_pressure, temperature):
@@ -39,16 +42,18 @@ def n_delta(wavelength, atmospheric_pressure, water_vapor_pressure, temperature)
 
 	# want wave number in units 1/micron
 	# wavelength is input in nm
-	wave_num=1E3 / wavelength
+	wave_num = 1E3 / wavelength
 
 	dry_air = (2371.34 + (683939.7 / (130. - np.power(wave_num, 2.))) + 
 			  (4547.3 / (38.9 - np.power(wave_num, 2.))))
-	wet_air = (6487.31 + 58.058 * np.power(wave_num, 2.) - 
-			  0.71150 * np.power(wave_num, 4.) +0.08851 * np.power(wave_num, 6.))
 
+	wet_air = (6487.31 + 58.058 * np.power(wave_num, 2.) + 
+			  -0.71150 * np.power(wave_num, 4.) +0.08851 * np.power(wave_num, 6.))
 
+	#print(dry_air, d_s(atmospheric_pressure, water_vapor_pressure, temperature))
+	#print(wet_air, d_w(water_vapor_pressure, temperature))
 	return (dry_air * d_s(atmospheric_pressure, water_vapor_pressure, temperature) +
-		wet_air * d_w(water_vapor_pressure, temperature))
+		    wet_air * d_w(water_vapor_pressure, temperature))
 
 
 def refraction(zenith_angle, wavelength, atmospheric_pressure, water_vapor_pressure, 
@@ -57,8 +62,8 @@ def refraction(zenith_angle, wavelength, atmospheric_pressure, water_vapor_press
 	reduced_n = n_delta(wavelength, atmospheric_pressure, water_vapor_pressure, temperature) * 1E-8
 
 	atmos_scale = 0.001254 * ((273.15 + temperature) / 273.15) # eqn 9 of Stone 1996
-	relative_gravity = (1. + 0.005302 * np.power(np.sin(np.radians(latitude)), 2.) + 
-		-0.00000583 * np.power(np.sin(np.radians(2.*latitude)), 2.) - 0.000000315 * altitude)
+	relative_gravity = (1. + 0.005302 * np.sin(np.radians(latitude))**2. + 
+		-0.00000583 * np.sin(np.radians(2.*latitude))**2. - 0.000000315 * altitude)
 
 	tanZ = np.tan(np.radians(zenith_angle))
 
@@ -75,7 +80,7 @@ def refraction(zenith_angle, wavelength, atmospheric_pressure, water_vapor_press
 		result = atmos_term_1 * tanZ + atmos_term_2 * np.power(tanZ, 3.)
 
 
-	return result
+	return np.degrees(result)
 
 
 
