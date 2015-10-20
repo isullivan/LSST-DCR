4543f2d654da5ab39f9651e4529b7bd7520e279f
David Reiss <reiss@uw.edu>
2015-10-19 17:44:36 -0700

    removed

--- code/try_from_S14DCR.py
+++
@@ -1,6 +0,0 @@
-#import lsst.sims.catalogs.measures.photometry.Sed as Sed
-#import lsst.sims.catalogs.measures.photometry.Bandpass as Bandpass
-
-import lsst.sims.photUtils.Sed as Sed
-import lsst.sims.photUtils.Bandpass as Bandpass
-
