ca16b2e9d6a26f11743d35b04b34e540e8985e75
Ian Sullivan <sullivan@D-173-250-205-150.dhcp4.washington.edu>
2015-12-02 10:48:23 -0800

    Update report with another reference, updated figure

--- DCR test code/calcRefractiveIndex.py
+++ DCR test code/calcRefractiveIndex.py
@@ -1,84 +1,38 @@
 from __future__ import print_function, division, absolute_import
 import numpy as np
 
-def p_s(atmospheric_pressure, water_vapor_pressure = 0.02):
-#
-# both atmospheric_pressure and water_vapor_pressure are input in atmospheres.
-# output is converted to millibars
-#
-
-	effective_pressure = 760. * 1.333224 * (atmospheric_pressure - water_vapor_pressure)
-
-	return effective_pressure
-
-def humidity_to_pressure(humidity=20., temperature=15.):
-	pascals_to_atm=1.0 / 101325.0
+def refraction(wavelength, zenith_angle, atmospheric_pressure, temperature, humidity=10, 
+	 latitude = -30.244639, altitude = 2663.):
+	"""
+	input pressure is in atmospheres
+	wavelength and zenith_angle may be supplied as numpy vectors
+	wavelength is in Angstroms (valid for 2302 < wavelength < 20586)
+	temperature is in Celcius (valid for -20 < T < 50)
+	humidity is in percent (0-100)
+	zenith angle is in degrees. Equations are valid for zenith_angle < 75
+	latitude is in degrees
+	altitude is in meters
+	"""
 	temperature_Kelvin = temperature + 273.15
-	saturation_pressure = (pascals_to_atm * np.exp(77.3450 + 0.0057 *temperature_Kelvin +
-						-7235.0 / temperature_Kelvin) / np.power(temperature_Kelvin, 8.2))
-	return (humidity/100.0) * saturation_pressure
+	water_vapor_pressure = humidity_to_pressure(humidity=humidity, temperature=temperature)
 
+	atm_to_millibar = 760. * 1.333224
+	dry_pressure = atm_to_millibar * atmospheric_pressure - water_vapor_pressure
 
-def p_w(water_vapor_pressure = 0.02):
-	water_vapor_pressure_millibar = 760. * 1.333224 * water_vapor_pressure
-	return water_vapor_pressure_millibar
+	reduced_n = n_delta(wavelength, dry_pressure, water_vapor_pressure, temperature_Kelvin) * 1E-8
 
+	atmos_scaleheight_ratio = 4.5908E-6 * temperature_Kelvin 
 
-def d_s(atmospheric_pressure, water_vapor_pressure, temperature):
-	# calculate dry air pressure term to refractive index calculation
-	T_use = 273.15 + temperature
-
-	eqn_1 = (1. + p_s(atmospheric_pressure, water_vapor_pressure) * 
-		(57.90E-8 - (9.3250E-4 / T_use) + (0.25844 / np.power(T_use, 2.))))
-
-	return eqn_1 * p_s(atmospheric_pressure, water_vapor_pressure) / T_use
-
-
-def d_w(water_vapor_pressure, temperature):
-	# calculate water vapor pressure term to refractive index calculation
-	T_use =	273.15 + temperature
-
-	eqn_1 = (-2.37321E-3 + (2.23366 / T_use) - (710.792 / np.power(T_use, 2.)) + 
-			(7.75141E-4 / np.power(T_use, 3.)))
-	eqn_2 = 1. + p_w(water_vapor_pressure) * (1. + 3.7E-4 * p_w(water_vapor_pressure)) * eqn_1
-
-	return eqn_2 * p_w(water_vapor_pressure) / T_use
-
-
-def n_delta(wavelength, atmospheric_pressure, water_vapor_pressure, temperature):
-	# calculate difference of refractive index of air from 1, multiplied by 1E8
-
-	# want wave number in units 1/micron
-	# wavelength is input in nm
-	wave_num = 1E3 / wavelength
-
-	dry_air = (2371.34 + (683939.7 / (130. - np.power(wave_num, 2.))) + 
-			  (4547.3 / (38.9 - np.power(wave_num, 2.))))
-
-	wet_air = (6487.31 + 58.058 * np.power(wave_num, 2.) + 
-			  -0.71150 * np.power(wave_num, 4.) +0.08851 * np.power(wave_num, 6.))
-
-	#print(dry_air, d_s(atmospheric_pressure, water_vapor_pressure, temperature))
-	#print(wet_air, d_w(water_vapor_pressure, temperature))
-	return (dry_air * d_s(atmospheric_pressure, water_vapor_pressure, temperature) +
-		    wet_air * d_w(water_vapor_pressure, temperature))
-
-
-def refraction(zenith_angle, wavelength, atmospheric_pressure, water_vapor_pressure, 
-	temperature, latitude = -30.244639, altitude = 2663.):
-	
-	reduced_n = n_delta(wavelength, atmospheric_pressure, water_vapor_pressure, temperature) * 1E-8
-
-	atmos_scale = 0.001254 * ((273.15 + temperature) / 273.15) # eqn 9 of Stone 1996
+	# Account for oblate Earth
 	relative_gravity = (1. + 0.005302 * np.sin(np.radians(latitude))**2. + 
 		-0.00000583 * np.sin(np.radians(2.*latitude))**2. - 0.000000315 * altitude)
 
 	tanZ = np.tan(np.radians(zenith_angle))
 
-	atmos_term_1 = reduced_n * relative_gravity * (1. - atmos_scale) 
-	atmos_term_2 = reduced_n * relative_gravity * (atmos_scale - reduced_n / 2.) 
+	atmos_term_1 = reduced_n * relative_gravity * (1. - atmos_scaleheight_ratio) 
+	atmos_term_2 = reduced_n * relative_gravity * (atmos_scaleheight_ratio - reduced_n / 2.) 
 	
-	if (type(zenith_angle) == np.ndarray) and (type(temperature) == np.ndarray):
+	if (type(zenith_angle) == np.ndarray) and (type(wavelength) == np.ndarray):
 		atmos_term_1 = np.matrix(atmos_term_1)
 		atmos_term_2 = np.matrix(atmos_term_2)
 		tanZ = np.matrix(tanZ)
@@ -91,18 +45,65 @@ def refraction(zenith_angle, wavelength, atmospheric_pressure, water_vapor_press
 	return np.degrees(result)
 
 
-
-def diff_refraction(zenith_angle, wavelength, bandwidth = None, atmospheric_pressure = 1., 
-	water_vapor_pressure = 0.02, temperature = 20. , latitude = -30.244639, altitude = 2663.):
+def diff_refraction(wavelength, zenith_angle, bandwidth = None, atmospheric_pressure = 1., 
+	humidity = 10., temperature = 20. , latitude = -30.244639, altitude = 2663.):
 	
 	if bandwidth is None: 
 		bandwidth = wavelength / 4.
 	wavelength_start = wavelength - bandwidth / 2.
 	wavelength_end = wavelength + bandwidth / 2.
 
-	refraction_start = refraction(zenith_angle, wavelength_start, atmospheric_pressure, water_vapor_pressure,
-		temperature, latitude, altitude)
-	refraction_end = refraction(zenith_angle, wavelength_end, atmospheric_pressure, water_vapor_pressure,
-		temperature, latitude, altitude)
+	refraction_start = refraction(wavelength_start, zenith_angle, atmospheric_pressure, 
+		temperature, humidity=humidity, latitude=latitude, altitude=altitude)
+	refraction_end = refraction(wavelength_end, zenith_angle, atmospheric_pressure, 
+		temperature, humidity=humidity, latitude=latitude, altitude=altitude)
 
 	return refraction_start - refraction_end	
+
+def n_delta(wavelength, dry_pressure, water_vapor_pressure=0.0, temperature=300.0):
+	"""
+	calculate difference of refractive index of air from 1, multiplied by 1E8
+
+	temperature is in units of Kelvin
+	pressures are in units of millibar
+	want wave number in units 1/micron
+	wavelength is input in Angstroms
+	"""
+	wave_num = 1E4 / wavelength
+
+	dry_air_term = (2371.34 + (683939.7 / (130. - np.power(wave_num, 2.))) + 
+			  (4547.3 / (38.9 - np.power(wave_num, 2.))))
+
+	wet_air_term = (6487.31 + 58.058 * np.power(wave_num, 2.) + 
+			  -0.71150 * np.power(wave_num, 4.) +0.08851 * np.power(wave_num, 6.))
+
+	#print(dry_air, density_factor_dry(atmospheric_pressure, water_vapor_pressure, temperature))
+	#print(wet_air, density_factor_water(water_vapor_pressure, temperature))
+	return (dry_air_term * density_factor_dry(dry_pressure, temperature) +
+		    wet_air_term * density_factor_water(water_vapor_pressure, temperature))
+
+
+def density_factor_dry(dry_pressure, temperature):
+	# calculate dry air pressure term to refractive index calculation
+
+	eqn_1 = (1. + dry_pressure * (57.90E-8 - (9.3250E-4 / temperature) + (0.25844 / np.power(temperature, 2.))))
+
+	return eqn_1 * dry_pressure / temperature
+
+
+def density_factor_water(water_vapor_pressure, temperature):
+	# calculate water vapor pressure term to refractive index calculation
+
+	eqn_1 = (-2.37321E-3 + (2.23366 / temperature) - (710.792 / np.power(temperature, 2.)) + 
+			(7.75141E-4 / np.power(temperature, 3.)))
+	eqn_2 = 1. + water_vapor_pressure * (1. + 3.7E-4 * water_vapor_pressure) * eqn_1
+
+	return eqn_2 * water_vapor_pressure / temperature
+
+
+def humidity_to_pressure(humidity=20., temperature=15.):
+	pascals_to_mbar=60. * 1.333224 / 101325.0
+	temperature_Kelvin = temperature + 273.15
+	saturation_pressure = (pascals_to_mbar * np.exp(77.3450 + 0.0057 *temperature_Kelvin +
+						-7235.0 / temperature_Kelvin) / np.power(temperature_Kelvin, 8.2))
+	return (humidity/100.0) * saturation_pressure
